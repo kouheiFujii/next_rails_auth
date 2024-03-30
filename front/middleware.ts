7ba1b662f1f735357a0ab5b3164d0013c9afef3d
kouheifujii <kohei.fujii@i-nations.tokyo>
2024-03-30 12:07:13 +0900

    feat: [front] base middleware.ts.

---
+++ front/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get("access_token");
+  const client = request.cookies.get("client");
+  const uid = request.cookies.get("uid");
+
+  if (accessToken && client && uid) {
+    return NextResponse.next();
+  } else {
+    const requestPath = request.nextUrl.pathname;
+    if (requestPath === "/login") {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
