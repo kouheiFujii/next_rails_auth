e3838973edf598aa2255c0193063246f261c4cae
kouheiFujii <kohei.fujii@i-nations.tokyo>
2024-04-13 16:50:34 +0900

    refactor:  middleware to skip certain paths if access token, client, and uid are present

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,21 +1,19 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const skipPaths = ["/login", "/signup"];
+
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get("access-token");
   const client = request.cookies.get("client");
   const uid = request.cookies.get("uid");
+  const response = NextResponse.next();
+  const requestPath = request.nextUrl.pathname;
 
-  if (accessToken && client && uid) {
-    return NextResponse.next();
-  } else {
-    const requestPath = request.nextUrl.pathname;
-    if (requestPath === "/login" || requestPath === "/signup") {
-      return NextResponse.next();
-    } else {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-  }
+  if (accessToken && client && uid) return response;
+  if (skipPaths.includes(requestPath)) return response;
+
+  return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
