7fd17da31534163c13c8491996b016b7e5864cac
kouheiFujii <kohei.fujii@i-nations.tokyo>
2024-04-19 22:24:45 +0900

    fix: Add user association to Todo model and migration

--- api/db/migrate/20240406035928_create_todos.rb
+++ api/db/migrate/20240406035928_create_todos.rb
@@ -1,6 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :title, null: false
       t.boolean :completed, default: false
 
